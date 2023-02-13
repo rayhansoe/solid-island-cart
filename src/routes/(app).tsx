06e974a1624e85c6e6ae070186180a3107a42cfe
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-14 04:43:41 +0700

    .

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -2,14 +2,27 @@ import type { VoidComponent } from "solid-js";
 import { batch } from "solid-js";
 import { Show } from "solid-js";
 import { Outlet, useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
 import NavBar from "~/components/NavBar";
 import CartContext from "~/context/CartContext";
 import { getServerCartItemsData$ } from "~/services/CartServices";
+import { prisma } from "~/server/db/client";
 
 export function routeData() {
 	const { setCartItems } = CartContext;
 
-	const cartItems = getServerCartItemsData$();
+	const cartItems = createServerData$(
+		async () => {
+			const cartItems = await prisma.cartItem.findMany({
+				select: { id: true, isChecked: true, productId: true, quantity: true, status: true },
+			});
+
+			return cartItems;
+		},
+		{
+			deferStream: true,
+		}
+	);
 
 	const data = cartItems();
 
