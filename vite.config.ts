be503faa7f235f4124909bd505983aca33c3e75c
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-14 22:38:28 +0700

    netlify.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import vercel from "solid-start-vercel";
-// import netlify from "solid-start-netlify";
+//
+// import vercel from "solid-start-vercel";
+import netlify from "solid-start-netlify";
 
 export default defineConfig(() => {
 	return {
 		plugins: [
 			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: false }) }),
-			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: false }) }),
-			solid({ islands: true, islandsRouter: true, ssr: true, adapter: vercel({ edge: false }) }),
+			solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: false }) }),
+			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: vercel({ edge: false }) }),
 		],
 		ssr: { external: ["@prisma/client"] },
 	};
