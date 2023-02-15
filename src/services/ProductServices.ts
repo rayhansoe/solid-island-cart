5bee6c0c331f3486c96a154e1edc8e3f867cd100
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-15 17:22:55 +0700

    update product service.

--- src/services/ProductServices.ts
+++ src/services/ProductServices.ts
@@ -292,21 +292,35 @@ export const updateProductPopularityRaw = async (
 
 // Decrease Product Stock
 export const decreaseProductsStock = async (prisma: prismaType) => {
-	const cartItems = await getCartItems(prisma);
-	const arrQuery = cartItems.map((item) =>
-		prisma.product.update({
-			where: {
-				id: item.productId,
-			},
-			data: {
-				stock: {
-					decrement: item.quantity,
-				},
-			},
-		})
-	);
+	return await prisma.$transaction(async (ctx) => {
+		const cartItems = await ctx.cartItem.findMany();
 
-	return await prisma.$transaction(arrQuery);
+		cartItems.forEach(async (item) => {
+			await ctx.product.update({
+				where: { id: item.productId },
+				data: {
+					stock: {
+						decrement: item.quantity,
+					},
+				},
+			});
+		});
+
+		return await ctx.product.findMany({
+			orderBy: {
+				popularity: "desc",
+			},
+			select: {
+				id: true,
+				name: true,
+				category: true,
+				stock: true,
+				price: true,
+				imgUrl: true,
+				popularity: true,
+			},
+		});
+	});
 };
 
 // Decrease Product Stock
