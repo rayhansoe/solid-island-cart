cd7195589641b8e3b67f8f65b1bcc7f72952ed7f
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-19 00:52:09 +0700

    edge...

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => {
 	return {
 		plugins: [
 			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: node({ edge: false }) }),
-			solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: false }) }),
+			solid({ islands: true, islandsRouter: true, ssr: true, adapter: netlify({ edge: true }) }),
 			// solid({ islands: true, islandsRouter: true, ssr: true, adapter: vercel({ edge: false }) }),
 		],
 		ssr: { external: ["@prisma/client"] },
